90472c1e8c020ed73d9e1ebaad15f418ca335a63
thomasZbnah <102034290+thomasZbnah@users.noreply.github.com>
2022-04-12 11:06:21 +0100

    commented code

--- assignment2.py
+++ assignment2.py
@@ -18,10 +18,11 @@ from numba import njit    # Have to import the "@njit" decorator from the Numba 
 from scipy import ndimage, misc
 from PIL import Image, ImageFilter
 
+#change resolution of displayed images
 mpl.rcParams['figure.dpi']= 150
 db = np.load("1-NN-descriptor-vects.npy")
 
-
+##################### functions definitions for labelling images
 def otsu(image):
     """Otsu thresholding of an 8-bit greyscale image.  Input must be an 8-bit
        greyscale image, returns Otsu threshold, k, and inter-class variance of 
@@ -96,15 +97,16 @@ def grow_region(im, L, r, c, curr_label):
 
 ###########################################################################
 
-image4 = plt.imread('image.png',False)
+image4 = plt.imread('image.png',False) #load the image as rgb image
 R,C = image4.shape[0],image4.shape[1]
 imutils.imshow(image4)
+#the image actually has four channels, so need to make a new 3 channels image
 image = np.zeros((image4.shape[0],image4.shape[1],3),dtype = "float32")
 image[...,0] = image4[...,0]
 image[...,1] = image4[...,1]
 image[...,2] = image4[...,2]
 imutils.imshow(image)
-hsv_image = color.rgb2hsv(image)
+hsv_image = color.rgb2hsv(image) #change image from rgb to hsv
 imutils.imshow(hsv_image[...,0],colourmap='hsv')
 imutils.imshow(hsv_image[...,1],colourmap='Greys') #cmap='gist_gray'
 imutils.imshow(hsv_image[...,2],colourmap='Greys')
@@ -112,53 +114,67 @@ imutils.imshow(hsv_image[...,2],colourmap='Greys')
 mask_image = np.zeros((image4.shape[0],image4.shape[1]),dtype = "uint8")
 for i in range(0,R):
     for j in range(0,C):
-        #if (hsv_image[i,j,0] >= 0.9) or (hsv_image[i,j,0] <= 0.1) and (hsv_image[i,j,1] >= 0.5):
+        #browse each pixel of the image and create a black and white image of only red and saturated pixels
         if (hsv_image[i,j,0] >= 0.95) or (hsv_image[i,j,0] <= 0.1) and (hsv_image[i,j,1] >= 0.5) : #good settings
             mask_image[i,j] = 255
 imutils.imshow(mask_image)
 
-filtmasked_image = ndimage.binary_erosion(mask_image, structure=np.ones((2,2))).astype(mask_image.dtype)
+#erosion filter to make the smallest groups of pixels disappear
+filtmasked_image = ndimage.binary_erosion(mask_image, structure=np.ones((2,2))).astype(mask_image.dtype) 
+#max filter to thicken the remaining groups of pixels so that a sign will appear as only one group, for later labelling
 filtmasked_image = ndimage.maximum_filter(filtmasked_image, size=5)
-
 imutils.imshow(filtmasked_image)
+
+#labbeling the image
 c_th, c_sB2 = otsu(filtmasked_image)
 image_binary = filtmasked_image > c_th
 image_lab = label_regions(image_binary)
 
+#browse the image per label and get the size of the biggest group of pixels
 maxsize = 0
 for i in range(1,np.max(image_lab)+1):
     if(np.nonzero(image_lab==i)[0].size>maxsize):
         maxsize = np.nonzero(image_lab==i)[0].size
 
+#this steps separated labeled signs form the rest of labeled pixels
 signs = []
+#this for loop processes every label in the image
 for lab in range(1, np.max(image_lab)+1):
+    #only take groups of pixels bigger than 35% of the biggest group
     if (np.nonzero(image_lab == lab)[0].size > maxsize*0.35):
+        #get the pixels coordinates of the square encompassing the sign
         a = np.min(np.nonzero(image_lab == lab)[0])
         b = np.max(np.nonzero(image_lab == lab)[0])
         c = np.min(np.nonzero(image_lab == lab)[1])
         d = np.max(np.nonzero(image_lab == lab)[1])
+        #only keeps squares having a ratio between 2/3 and 3/2
         if ((b-a)/(d-c) > 2/3) and ((b-a)/(d-c) < 3/2):
             zero = np.zeros((b-a, d-c, 3), dtype="float32")
-            print("zob")
             for i in range(a, b):
                 for j in range(c, d):
                     zero[i-a, j-c, 0] = image[i, j, 0]
                     zero[i-a, j-c, 1] = image[i, j, 1]
                     zero[i-a, j-c, 2] = image[i, j, 2]
+            #resulting signs are stored in this signs list
             signs.append(zero)
             imutils.imshow(zero)
-            print("append")
 
+#this loops processes each sign of the signs list for pre processing before using the rol classifier
 for sign in signs:
+    #signs become gray
     sign  = color.rgb2gray(sign)
+    #then are resized to 64 by 64 image
     sign = sktrsfm.resize(sign, (64,64))
-    print(sign.shape)
+    #then the image is converted from float32 to uint8
     sign *= 255
     sign = sign.astype(np.uint8)
+    #images get a contrast enhancement
     enhanced_sign = np.zeros(sign.shape, dtype = 'uint8')
     np.clip(sign * 1.1, 0, 255, enhanced_sign)
+    #the mean pixel value is removed to eveyr pixel of the image
     mean_val = np.average(enhanced_sign)
     enhanced_sign -= mean_val.astype('uint8')
+    #the image is transformed from a 64x64 matrix to a 4096 dimensions vector
     enhanced_sign = enhanced_sign.flatten()
     
     
